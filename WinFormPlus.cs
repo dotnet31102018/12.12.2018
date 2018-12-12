3a279dd54ccf19681e2c42649a956ba771f4f203
dotnet31102018 <44984976+dotnet31102018@users.noreply.github.com>
2018-12-12 19:27:20 +0200

    Add files via upload

---
+++ WinFormPlus.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _1212WinForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void plus_Click(object sender, EventArgs e)
+        {
+            Debug.WriteLine("Clicked button");
+
+            Debug.WriteLine($"a Text box = {Convert.ToInt32(aTxtBox.Text)}");
+
+            Debug.WriteLine($"b Text box = {Convert.ToInt32(bTxtBox.Text)}");
+
+            int a = Convert.ToInt32(aTxtBox.Text);
+
+            int b = Convert.ToInt32(bTxtBox.Text);
+
+            int c = a + b;
+
+            resultLbl.Text = $"result = {c}";
+
+        }
+    }
+}
